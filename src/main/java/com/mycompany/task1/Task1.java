f08527cc4decb6b409d9421ca5e8cc710fa43b11
Nichita777 <sba24331@student.cct.ie>
2024-12-20 15:35:45 +0000

    1st commit

--- src/main/java/com/mycompany/task1/Task1.java
+++ src/main/java/com/mycompany/task1/Task1.java
@@ -1,16 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.task1;
 
-/**
- *
- * @author nichi
- */
-public class Task1 {
+import java.util.HashSet;
+import java.util.Scanner;
 
+public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        // Input array size
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        // Input array elements
+        int[] array = new int[size];
+        System.out.print("Enter the elements of the array: ");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        // Find the first repeated element
+        HashSet<Integer> seenElements = new HashSet<>();
+        int firstRepeated = -1;
+        for (int num : array) {
+            if (seenElements.contains(num)) {
+                firstRepeated = num;
+                break;
+            } else {
+                seenElements.add(num);
+            }
+        }
+
+        // Output the result
+        if (firstRepeated != -1) {
+            System.out.println("First repeated element: " + firstRepeated);
+        } else {
+            System.out.println("No repeated elements found.");
+        }
+
+        scanner.close();
     }
 }
